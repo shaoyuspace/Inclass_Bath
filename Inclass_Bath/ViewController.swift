76ada156bccbc45fb1262a81b5065c8403ece330
uuinclass <shaoyuspace@gmail.com>
2016-05-24 22:47:31 +0100

    Initial Commit

---
+++ Inclass_Bath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inclass_Bath
+//
+//  Created by Yu Shao on 16/5/24.
+//  Copyright © 2016年 Yu Shao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
